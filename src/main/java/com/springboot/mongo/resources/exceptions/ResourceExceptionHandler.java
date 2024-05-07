9be9ad872dcd3ab52c495f67d695e3b7738db942
Sávio Marques de Souza <saviomarquessouza@gmail.com>
2024-05-07 16:33:47 -0300

    Find by Id and Exceptions

---
+++ src/main/java/com/springboot/mongo/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.springboot.mongo.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.springboot.mongo.services.exception.ObjectNotFoundException;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		
+		String error = "UserNotFound";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+		
+	}
+	
+//	@ExceptionHandler(DataBaseException.class)
+//	public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request) {
+//		
+//		String error = "Data Base Error";
+//		HttpStatus status = HttpStatus.BAD_REQUEST;
+//		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+//		return ResponseEntity.status(status).body(err);
+//		
+//	}
+}
