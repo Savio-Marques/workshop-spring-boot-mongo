2e1e4c445388307c19327d16578006f0acd495e9
Sávio Marques de Souza <saviomarquessouza@gmail.com>
2024-05-13 10:06:23 -0300

    User deleter

--- src/main/java/com/springboot/mongo/resources/UserResource.java
+++ src/main/java/com/springboot/mongo/resources/UserResource.java
@@ -1,14 +1,19 @@
 package com.springboot.mongo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.mongo.domain.User;
 import com.springboot.mongo.dto.UserDTO;
@@ -30,8 +35,22 @@ public class UserResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		User user = service.FindById(id);
+		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) {
+		User user= service.fromDTO(objDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
--- src/main/java/com/springboot/mongo/services/UserService.java
+++ src/main/java/com/springboot/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.mongo.domain.User;
+import com.springboot.mongo.dto.UserDTO;
 import com.springboot.mongo.repository.UserRepository;
 import com.springboot.mongo.services.exception.ObjectNotFoundException;
 
@@ -20,10 +21,22 @@ public class UserService {
 		return repo.findAll();
 	}
 	
-	public User FindById(String id) {
+	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("User not found"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User fromDTO(UserDTO obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
+	}
 	
 }
